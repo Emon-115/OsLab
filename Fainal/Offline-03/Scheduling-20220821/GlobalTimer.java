aba0a5d4235facbc4d899fad4c1dc548a7828cf1
Emon Ahmed <88421405+Emon-115@users.noreply.github.com>
2022-10-20 11:31:17 +0600

    Add files via upload

---
+++ Fainal/Offline-03/Scheduling-20220821/GlobalTimer.java
@@ -0,0 +1,14 @@
+package schedulingos;
+
+public class GlobalTimer {
+    private int time;
+    public GlobalTimer(int initialTime){
+        this.time=initialTime;
+    }
+    public void TimeAdvance(){
+        this.time += 1;
+    }
+    public int getGlobalTime(){
+        return this.time;
+    }
+}
